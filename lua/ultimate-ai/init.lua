77b2aa79913214ec9ab289abe1f6aecc9c4c6dd2
UltimateMaster000 <riley_000@icloud.com>
2026-07-14 16:29:36 +1000

    Added test popupgit status

--- lua/ultimate-ai/init.lua
+++ lua/ultimate-ai/init.lua
@@ -12,6 +12,8 @@ end
 function M.run_subcommand(subcmd)
   if subcmd == "test" then
     M.test()
+  elseif subcmd == "popup" then
+    M.MyMenu()
   else
     print("Unknown command " .. (subcmd or ""))
   end
@@ -19,6 +21,39 @@ end
 
 function M.test()
   print("Test passed successfully!")
+end
+
+function ShowPopup(opts, callback)
+  local popup = require("plenary.popup")
+
+  local Window_id
+
+  local height = 20
+  local width = 30
+  local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+  Window_id = popup.create(opts, {
+    title = "UltimateAI",
+    highlight = "MyProjectWindow",
+    line = math.floor(((vim.o.lines - height) / 2) - 1),
+    col = math.floor((vim.o.columns - width) / 2),
+    minwidth = width,
+    minheight = height,
+    borderchars = borderchars,
+    callback = callback,
+  })
+end
+
+function MyMenu()
+  local opts = {}
+  local cb = function(_, sel)
+    print("it works")
+  end
+  ShowPopup(opts, cb)
+end
+
+function CloseMenu()
+  vim.api.nvim_win_close(Win_id, true)
 end
 
 
