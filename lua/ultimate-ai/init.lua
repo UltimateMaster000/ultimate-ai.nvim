6a31e8fda9c3e98e129e0ebd4ec6bc5f3c538e33
UltimateMaster000 <riley_000@icloud.com>
2026-07-14 15:17:09 +1000

    added test

--- lua/ultimate-ai/init.lua
+++ lua/ultimate-ai/init.lua
@@ -3,6 +3,7 @@ local M = {}
 M.defaults = {}
 
 function M.setup(opts)
+  print("hello from setup")
   M.config = vim.tbl_deep_extend("force", M.defaults, opts or {})
   vim.api.nvim_create_user_command("sayHello", function()
     M.say_hello()
